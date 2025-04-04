2c8fd4e89a32774fce6d3252274371bfd5d354cf
LittleSheep <littlesheep.code@hotmail.com>
2025-04-05 01:13:20 +0800

    :wastebasket: Remove unused admin api

--- pkg/internal/http/admin/index.go
+++ pkg/internal/http/admin/index.go
@@ -5,6 +5,5 @@ import "github.com/gofiber/fiber/v2"
 func MapControllers(app *fiber.App, baseURL string) {
 	admin := app.Group(baseURL)
 	{
-		admin.Post("/fediverse", adminTriggerFediverseFetch)
 	}
 }
